041b46a9ca002f1d7e9a5f5fd71fe6995d623706
veraAlan <alan.vera@est.fi.uncoma.edu.ar>
2022-08-22 14:47:00 -0300

    Started E3&E4.

--- TP2/TP1Validations/E3&E4/View/index.php
+++ TP2/TP1Validations/E3&E4/View/index.php
@@ -1,49 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Ref -->
+    <link href="../../../Libraries/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../../Libraries/Bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <title>Document</title>
 </head>
 
 <style>
-    input{
+    input {
         margin: 10px 0px;
         border-color: 0.7px solid gray;
     }
-
 </style>
 
 <body>
     <h1>Ingrese sus datos.</h1>
-    <br/>
-    <form action="../Controller/presentacion.php" method="GET" onsubmit="return validate()">
-        Nombre: <input type="text" name="nombre" id="nombre"><br/>
-        Apellido: <input type="text" name="apellido" id="apellido"><br/>
-        Edad: <input type="number" name="edad" id="edad"><br/>
-        Direccion: <input type="text" name="direccion" id="direccion"><br/>
+    <br />
 
-        <input type="submit" value="Enviar" name="submit"> <!-- onclick="return validate()" -->
-    </form>
+    <div class="container-flex m-2 w-25">
+        <form class="needs-validation" action="../Controller/presentacion.php" method="POST">
+            <div class="col-auto">
+                <label for="name">Nombre: </label>
+                <input type="text" name="name" id="name" required>
+                <div class="invalid-feedback">
+                    Ingrese un nombre válido.
+                </div>
+            </div>
+            <div class="col-auto">
+                <label for="surname">Apellido: </label>
+                <input type="text" name="surname" id="surname" required>
+                <div class="invalid-feedback">
+                    Ingrese un apellido válido.
+                </div>
+            </div>
+            <div class="col-auto">
+                <label for="age">Edad: </label>
+                <input type="text" name="age" id="age" required>
+                <div class="invalid-feedback">
+                    Ingrese una edad.
+                </div>
+            </div>
+            <div class="col-auto">
+                <label for="dir">Direccion: </label>
+                <input type="text" name="dir" id="dir" required>
+                <div class="invalid-feedback">
+                    Ingrese una dirección.
+                </div>
+            </div>
+            <div class="col-auto">
+                <button class="btn btn-primary" type="submit">Enviar datos</button>
+            </div>
+        </form>
+    </div>
 
     <script type="text/javascript">
-        function validate(){
-            names = ["nombre", "apellido", "edad", "direccion"];
-            verified = true;
+        (() => {
+            'use strict'
 
-            // Iterates through all inputs and checks if something was inputted.
-            for(i = 0; i < 4; i++){
-                if(document.getElementById(names[i]).value == ""){
-                    document.getElementById(names[i]).style.borderColor = "red";
-                    verified = false;
-                } else {
-                    document.getElementById(names[i]).style.borderColor = "";
-                }
-            }
+            const forms = document.querySelectorAll('.needs-validation')
 
-            return verified;
-        }
+            Array.form(forms).forEach(form => {
+                form.addEventListener('submit', event => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+        })()
     </script>
 </body>
+
 </html>

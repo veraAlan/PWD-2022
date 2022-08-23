6177696dabd9057fb67f87d48b490b3117247836
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2022-08-23 19:53:27 -0300

    Completed E8.

--- TP2/TP1Validations/E8/Controller/precio.php
+++ TP2/TP1Validations/E8/Controller/precio.php
@@ -2,23 +2,25 @@
 switch ($_GET['student']) {
     case "yes":
         if ($_GET['age'] >= 12) {
-            echo "El precio es $180.<br/>";
+            echo "<h3>El precio es $180.</h3>";
         } else {
-            echo "El precio es $160.<br/>";
+            echo "<h3>El precio es $160.</h3>";
         }
         break;
     case "no":
         if ($_GET['age'] < 12) {
-            echo "El precio es $160.<br/>";
+            echo "<h3>El precio es $160.</h3>";
         } else {
-            echo "El precio es $300.<br/>";
+            echo "<h3>El precio es $300.</h3>";
         }
         break;
 }
 
-// Back button.
-echo '<br/>
-    <form action="../View/index.php">
-        <input type="submit" value="Volver a consultar" />
-    </form>
-    ';
+// Back button
+echo '<br/><br/>
+        <!-- Bootstrap Ref -->
+        <link href="../../../Libraries/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <script src="../../../Libraries/Bootstrap/js/bootstrap.min.js" type="text/javascript"></script> 
+        <form action="../View/index.php">
+            <button class="btn btn-primary" type="submit" name="submit">Volver</button>
+        </form>';
--- TP2/TP1Validations/E8/View/index.php
+++ TP2/TP1Validations/E8/View/index.php
@@ -5,18 +5,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Ref -->
+    <link href="../../../Libraries/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../../Libraries/Bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <title>Cine Cinem@s</title>
 </head>
 
 <body>
-    <form action="../Controller/precio.php" method="GET">
-        Edad: <input type="number" id="age" name="age" value="0"><br />
-        Es un estudiante: <select id="student" name="student">
-            <option value="yes">Si</option>
-            <option value="no">No</option>
-        </select><br />
-        <input type="submit" value="Calcular precio...">
-    </form>
+    <div class="container-md w-auto">
+        <h2 class="py-4">Precios de las entradas de Cinem@s.</h2>
+        <form class="needs-validation row g-2" action="../Controller/precio.php" method="GET">
+            <div class="col-md">
+                <label for="ageValidation" class="form-label">Edad</label>
+                <input type="number" class="form-control" id="age" name="age" required>
+            </div>
+            <div class="col-md-6">
+                <label for="studentValidation" class="form-label">Es un estudiante?</label>
+                <select class="form-select" id="student" name="student" required>
+                    <option selected disabled value="">Elegir...</option>
+                    <option value="yes">Si</option>
+                    <option value="no">No</option>
+                </select>
+            </div>
+            <div class="col-md-12">
+                <button class="btn btn-primary w-100" type="submit">Calcular...</button>
+            </div>
+        </form>
+    </div>
+
+    <script type="text/javascript">
+        (() => {
+            'use strict'
+
+            const forms = document.querySelectorAll('.needs-validation')
+
+            Array.form(forms).forEach(form => {
+                form.addEventListener('submit', event => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+
+        })()
+    </script>
 </body>
 
 </html>

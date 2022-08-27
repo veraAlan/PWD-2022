5990b33b0d76927807d3624b5d1eb783d6193ae6
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2022-08-27 04:30:34 -0300

    Changed Diretories
    Removed unnecesary files and badly placed directories.

--- TP2/TP1Validations/E3&E4/Controller/presentacion.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Hola, yo soy " . $_POST['nombre'] . " " . $_POST['apellido'] . " tengo " . $_POST['edad'] . " años y vivo en " . $_POST['direccion'] . "<br/>";
-if ($_POST['edad'] >= 18) {
-    echo "Y soy mayor de edad.";
-} else {
-    echo "Y soy menor de edad.";
-}

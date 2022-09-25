ed13da761144abd56bc26a074364eaf4b10dbf80
acostaDemianAaron <acostademiann14@gmail.com>
2022-09-25 18:46:55 -0300

    add data_submitted

--- Entregables/phpMysql/Utils/functions.php
+++ Entregables/phpMysql/Utils/functions.php
@@ -1,1 +1,12 @@
 <?php
+
+function data_submitted()
+{
+    $AUX = [];
+    if (!empty($_POST)) {
+        $AUX = $_POST;
+    } elseif (!empty($_GET)) {
+        $AUX = $_GET;
+    }
+    return $AUX;
+}

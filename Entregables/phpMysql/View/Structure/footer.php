63fcabf45a3f1f856a0f745a0c054f77eaf38747
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2022-09-26 02:16:26 -0300

    Corrected footer and header

--- Entregables/phpMysql/View/Structure/footer.php
+++ Entregables/phpMysql/View/Structure/footer.php
@@ -1,3 +1,23 @@
+<?php
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $url = "https://";
+} else {
+    $url = "http://";
+    $url .= $_SERVER['HTTP_HOST'];
+    $url .= $_SERVER['REQUEST_URI'];
+}
+$url_components = parse_url($url);
+$array_url = explode("/", $url_components['path']);
+array_pop($array_url);
+for ($i = 0; $i < 3; $i++) {
+    array_shift($array_url);
+}
+$shift = "";
+while (count($array_url) > 1) {
+    $shift .= "../";
+    array_pop($array_url);
+};
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +26,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
-    <link rel="stylesheet" href="Assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo $shift ?>View/Assets/css/style.css">
 </head>
 
 <body>
--- Entregables/phpMysql/View/Structure/header.php
+++ Entregables/phpMysql/View/Structure/header.php
@@ -1,10 +1,31 @@
-<script src="Assets/js/jquery-3.6.1.min.js"></script>
-<script src="Assets/js/bootstrap.bundlev5.2.1.min.js"></script>
-<script src="Assets/js/isotope.min.js"></script>
-<script src="Assets/js/owl-carousel.js"></script>
-<script src="Assets/js/tabs.js"></script>
-<script src="Assets/js/popup.js"></script>
-<script src="Assets/js/custom.js"></script>
+<?php
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $url = "https://";
+} else {
+    $url = "http://";
+    $url .= $_SERVER['HTTP_HOST'];
+    $url .= $_SERVER['REQUEST_URI'];
+}
+$url_components = parse_url($url);
+$array_url = explode("/", $url_components['path']);
+array_pop($array_url);
+for ($i = 0; $i < 3; $i++) {
+    array_shift($array_url);
+}
+$shift = "";
+while (count($array_url) > 1) {
+    $shift .= "../";
+    array_pop($array_url);
+};
+?>
+
+<script src="<?php echo $shift ?>View/Assets/js/jquery-3.6.1.min.js"></script>
+<script src="<?php echo $shift ?>View/Assets/js/bootstrap.bundlev5.2.1.min.js"></script>
+<script src="<?php echo $shift ?>View/Assets/js/isotope.min.js"></script>
+<script src="<?php echo $shift ?>View/Assets/js/owl-carousel.js"></script>
+<script src="<?php echo $shift ?>View/Assets/js/tabs.js"></script>
+<script src="<?php echo $shift ?>View/Assets/js/popup.js"></script>
+<script src="<?php echo $shift ?>View/Assets/js/custom.js"></script>
 <!-- ***** Header Area Start ***** -->
 <header class="header-area header-sticky">
     <div class="container">
@@ -13,7 +34,7 @@
                 <nav class="main-nav">
                     <!-- ***** Logo Start ***** -->
                     <a href="index.html" class="logo">
-                        <img src="Assets/img/logo.svg" alt="">
+                        <img src="<?php echo $shift ?>View/Assets/img/logo.svg" alt="">
                     </a>
                     <!-- ***** Logo End ***** -->
                     <!-- ***** Menu Start ***** -->

65c68728cddb5571396593e1d798b30432413d96
acostaDemianAaron <acostademiann14@gmail.com>
2022-09-25 22:06:15 -0300

    add Bootstrap and jQuery

---
+++ Entregables/phpMysql/View/Structure/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../Assets/css/bootstrapv5.2.1.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include_once("./header.php");
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-content">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include_once("./footer.php");
+    ?>
+</body>
+
+</html>

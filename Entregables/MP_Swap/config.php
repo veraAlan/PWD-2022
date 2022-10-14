b12b676eef5cc62b66f92ea4cf8c0547603cd42e
Alan Cristian Gaston Vera <93807967+veraAlan@users.noreply.github.com>
2022-10-14 03:15:19 -0300

    Test

--- Entregables/MP_Swap/config.php
+++ Entregables/MP_Swap/config.php
@@ -1,3 +1,12 @@
 <?php
 
 // Set project variables.
+
+$PROJECT = 'PWD-2022/Entregables/MP_Swap';
+
+$ROOT = $_SERVER['DOCUMENT_ROOT'] . "/$PROJECT/";
+
+// TODO Definir pagina principal.
+$INDEX = 'Location:http://' . $_SERVER['HTTP_HOST'] . "/$PROJECT/index.php";
+
+$_SESSION['ROOT'] = $ROOT;

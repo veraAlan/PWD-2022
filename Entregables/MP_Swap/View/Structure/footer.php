7c1b296a5688a24a8e18a9d5f2e3127ac1ffcffa
acostaDemianAaron <acostademiann14@gmail.com>
2022-10-19 23:25:27 -0300

    menu

---
+++ Entregables/MP_Swap/View/Structure/footer.php
@@ -0,0 +1,8 @@
+<footer class="col-sm-12 d-flex justify-content-center bg-dark">
+    <p class="text-white mt-2 mb-2">© 2022 VASA</p>
+  </footer>
+
+<script src="../Assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ Entregables/MP_Swap/View/Structure/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../Assets/css/style.css">
+    <title>Menu</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
+            <div class="container-fluid">
+                <div>
+                    <img class="me-3" src="../Assets/img/logo.svg" alt="" width="75px">
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                </div>
+            </div>
+        </nav>
+    </header>
